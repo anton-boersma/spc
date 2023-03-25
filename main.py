98a00815bed5e443f4cb8159014520d070027360
Anton <alj.boersma2408@gmail.com>
2023-03-25 15:13:21 +0100

    LED fader

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from machine import Pin
+from machine import Pin, PWM
 from utime import sleep_ms
 
 led_pin = Pin("GP15", Pin.OUT)
-enabled = False
+led_pwm = PWM(led_pin)
+duty = 0
+increment = 1
 
 while True:
-    if enabled:
-        led_pin.low()
-        enabled = False
-    else:
-        led_pin.high()
-        enabled = True
-    sleep_ms(500)
-    # print(enabled)
+    led_pwm.duty_u16(duty)
+    if duty > 2**16 - 1:
+        increment = -1
+    elif duty < 1:
+        increment = 1
 
+    duty += increment
+# led_pin.low()
