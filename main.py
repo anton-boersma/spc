15fff3b671451e4e6faa41057888682b87b19ca9
Anton <alj.boersma2408@gmail.com>
2023-04-13 12:18:11 +0200

    Communication test v0.3

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ class POTMeter(Module):
     def get_output(self):
         global POTMeter_output
         POTMeter_output = self.value
+        # print("The POT value is:", self.value)
 
 
 class LEDFader(Module):
@@ -64,20 +65,77 @@ class LEDFader(Module):
         return POTMeter_output
 
 
+class VehicleTest(Module):
+    def __init__(self, left_pin_name, middle_pin_name, right_pin_name):
+        self.left_pin = Pin(left_pin_name, Pin.OUT)
+        self.middle_pin = Pin(middle_pin_name, Pin.OUT)
+        self.right_pin = Pin(right_pin_name, Pin.IN)
+
+        self.left_pwm = PWM(self.left_pin)
+        self.middle_pwm = PWM(self.middle_pin)
+        self.right_pwm = PWM(self.middle_pin)
+
+        self.left_duty = 0
+        self.middle_duty = 0
+        self.right_duty = 0
+
+        self.input = 0
+
+        self.current_time = ticks_us()
+        self.previous_time = self.current_time
+
+    def update(self):
+        self.input = VehicleTest.get_input(self)
+
+        if self.input < 21845:
+            self.left_duty = self.input * 3
+            self.middle_duty = 0
+            self.right_duty = 0
+        elif 21845 <= self.input < 43690:
+            self.left_duty = 2**16 - 2
+            self.middle_duty = (self.input - 21845) * 3
+            self.right_duty = 0
+        elif 43690 <= self.input < 65535:
+            self.left_duty = 2**16 - 2
+            self.middle_duty = 2**16 - 2
+            self.right_duty = (self.input - 43690) * 3
+        else:
+            self.left_duty = 2**16 - 2
+            self.middle_duty = 2**16 - 2
+            self.right_duty = 2**16 - 2
+
+        self.left_pwm.duty_u16(self.left_duty)
+        self.middle_pwm.duty_u16(self.middle_duty)
+        self.right_pwm.duty_u16(self.right_duty)
+
+        self.current_time = ticks_us()
+        if ticks_diff(self.current_time, self.previous_time) >= 2000000:  # time in micro seconds, us
+            print('Green value:', self.left_duty)
+            print('Yellow value:', self.middle_duty)
+            print('Red value:', self.right_duty)
+            print()
+            print('POTMeter value', VehicleTest.get_input(self))
+            print()
+
+            self.previous_time = self.current_time
+
+    def get_input(self):
+        global POTMeter_output
+        return POTMeter_output
+
+
 modules = [
     POTMeter(28),  # pot pin number GP28 or ADC2
-    LEDFader(10),
-    LEDFader(11),
-    LEDFader(12)
+    VehicleTest(10, 11, 12)  # LED pin numbers GP10 GP11 GP12, Pico pins 14 15 16
 ]
 
 POTMeter_output = 0
 sleep_ms(2000)  # pause before startup
-print("Modules loaded")
+print("Modules loaded, program is running.")
 
 while True:
     for module in modules:
         module.update()
-        module.get_output()
+        # module.get_output()
 
     # print(POTMeter_output)
