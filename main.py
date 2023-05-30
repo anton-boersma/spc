d4369d2f2266cb0f5b323d71f40e035a0d753838
Anton <alj.boersma2408@gmail.com>
2023-05-30 14:11:01 +0200

    Refactored v0.1.2 pre OLED

--- main.py
+++ main.py
@@ -116,6 +116,28 @@ class LEDFader(Module):
             self._previous_time = current_time
 
 
+class Display(Module):
+    def __init__(self):
+        from machine import Pin, I2C
+        from lib.ssd1306 import SSD1306_I2C
+        # Opzetten I2C protocol op pinnen
+        oled_i2c = I2C(0, sda=Pin(8), scl=Pin(9))
+        # Check of via i2c een chip te vinden is
+        print(oled_i2c.scan())
+        # I2C configureren voor ssd1306 display
+        oled = SSD1306_I2C(width=128, height=32, i2c=oled_i2c, addr=0x3D)
+        # Nu kunnen we gebruik maken van de functies die beschikbaar zijn
+        # voor het SSD1306 object, inclusief Framebuffer
+        oled.fill(0)
+        oled.text('Hello', 0, 0, 0xffff)
+        oled.text('MicroPython!', 0, 10, 0xffff)
+        oled.hline(0, 20, 95, 0xffff)
+        oled.show()
+
+    def update(self):
+        pass
+
+
 def main():
 
     sleep_ms(5000)  # pause before startup
