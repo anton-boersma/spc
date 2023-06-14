5a040c9fcbba8d0ddd9afb04bfadc610f94dad3f
Anton <alj.boersma2408@gmail.com>
2023-06-14 16:11:00 +0200

    End product v0.2.0 drivetrain started

--- modules/drivetrain_module.py
+++ modules/drivetrain_module.py
@@ -1,10 +1,16 @@
 from .module import Module
 from components.pot_meter import POTMeter
+from machine import Pin, PWM
+from utime import ticks_us, ticks_diff
 
 
 class DrivetrainModule(Module):
-    def __init__(self):
-        pass
+    def __init__(self, lv_pin_name: int, rv_pin_name: int, la_pin_name: int, ra_pin_name: int, pot_meter: POTMeter):
+        self._lv_pin = Pin(lv_pin_name, Pin.OUT)
+        self._rv_pin = Pin(rv_pin_name, Pin.OUT)
+        self._la_pin = Pin(la_pin_name, Pin.OUT)
+        self._ra_pin = Pin(ra_pin_name, Pin.OUT)
+        self._pot_meter = pot_meter
 
     def update(self):
         pass
